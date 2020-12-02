3a394f437f207f886eec5fd9b7f4482965071733
kainacrow <33405173+kainacrow@users.noreply.github.com>
2020-12-02 00:55:49 -0500

    removing hardcoded params, adding handleFormChange for submit button, updating method to grab params from window, updating setState, updating function to push objects to row array, inserting input area text fields and selects for user input, parsing numbers to 2 or 3 digits after decimal and adding percents or dollar signs

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import CurrencyTextField from '@unicef/material-ui-currency-textfield'
 
 export default class App extends Component {
 
     state = {
         loading: false,
+        loanSize: '',
+        creditScore: '',
+        propertyType: '',
+        occupancy: '',
         quotes: {
             lenderName: {
                 value: '',
@@ -33,48 +43,100 @@ export default class App extends Component {
             },
         }
     }
-    async componentDidMount() {
 
-        const url = new URL('https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes');
-        const params = { loanSize: 450000, creditScore: 680, propertyType: 'SingleFamily', occupancy: 'Primary' };
-        url.search = new URLSearchParams(params).toString();
+    handleFormChange = (e) => {
+    }
+
+    async componentDidMount() {
+        this.handleFormChange = this.handleFormChange.bind(this)
 
         const key = 'OU-AUTH ' + process.env.REACT_APP_API_KEY;
+        const url = new URL('https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes');
+        url.search = new URLSearchParams(window.location.search);
+        const urlParams = new URLSearchParams(window.location.search);
+        const params = {
+            loanSize: urlParams.get('loanSize'),
+            creditScore: urlParams.get('creditScore'),
+            propertyType: urlParams.get('propertyType'),
+            occupancy: urlParams.get('occupancy') }
         const response = await fetch(url, {
                 method: 'GET',
                 headers: {
                     Authorization: key,
                 },
             }
-        );
+        )
         const data = await response.json();
-
-        this.setState({
-            apr: data.rateQuotes.map(a => a.apr),
-            inputData: params,
-            closingCosts: data.rateQuotes.map(cc => cc.closingCosts),
-            interestRate: data.rateQuotes.map(ir => ir.interestRate),
-            lenderName: data.rateQuotes.map(ln => ln.lenderName),
-            loanType: data.rateQuotes.map(lt => lt.loanType),
-            monthlyPayment: data.rateQuotes.map(mp => mp.monthlyPayment),
-            quotes: data.rateQuotes ,
-        })
+        if(data.rateQuotes) {
+            this.setState({
+                apr: data.rateQuotes.map(a => a.apr),
+                params: params,
+                quotes: data.rateQuotes,
+            })
+        }
     }
     render() {
         const rows = []
-        for(var i=0; i<=this.state.quotes.length; i++) {
+        for(var i=0; i<this.state.quotes.length; i++) {
             rows.push(this.state.quotes[i])
-            i += 1
         }
         return (
-            <div>
-                <form>
-                    <label>
+            <form onChange={this.handleFormChange}>
+                <FormControl style={ {marginRight: 100, marginLeft: 100, marginBottom: 200, width: 100} }>
+                    <InputLabel>
                         Loan Size:
-                        <input type="text" name="loanSize" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
+                        <TextField
+                            type="number"
+                            name="loanSize"
+                        />
+                    </InputLabel>
+                </FormControl>
+                <FormControl style={ {marginRight: 100, marginLeft: 100, marginBottom: 200, width: 100} }>
+                    <InputLabel>
+                        Credit Score:
+                        <TextField
+                            type="number"
+                            name="creditScore"
+                            InputProps={{
+                                inputProps: {
+                                    max: 800, min: 300
+                                }
+                            }}
+                        />
+                    </InputLabel>
+                </FormControl>
+                <FormControl style={ {marginRight: 100, marginLeft: 100, marginBottom: 200} }>
+                    <InputLabel>
+                        Property Type:
+                        <Select
+                            name="propertyType"
+                            style={ {width: 120} }
+                        >
+                            <MenuItem value={'SingleFamily'}>SingleFamily</MenuItem>
+                            <MenuItem value={'Condo'} id>Condo</MenuItem>
+                            <MenuItem value={'Townhouse'}>Townhouse</MenuItem>
+                            <MenuItem value={'MultiFamily'}>MultiFamily</MenuItem>
+                        </Select>
+                    </InputLabel>
+                </FormControl>
+                <FormControl style={ {marginRight: 100, marginLeft: 100} }>
+                    <InputLabel>
+                        Occupancy:
+                        <Select
+                            name="occupancy"
+                            style={ {width: 150} }
+                        >
+                            <MenuItem value={'Primary'}>Primary</MenuItem>
+                            <MenuItem value={'Secondary'}>Secondary</MenuItem>
+                            <MenuItem value={'Investment'}>Investment</MenuItem>
+                        </Select>
+                    </InputLabel>
+                </FormControl>
+                <div>
+                    <FormControl style={ {marginLeft: 100, marginBottom: 100} }>
+                        <input type="submit" value="Submit" />
+                    </FormControl>
+                </div>
 
                 <TableContainer component={Paper}>
                     <Table aria-label="simple table">
@@ -82,8 +144,8 @@ export default class App extends Component {
                             <TableRow>
                                 <TableCell>Lender Name</TableCell>
                                 <TableCell align="right">Loan Type</TableCell>
-                                <TableCell align="right">Closing Costs</TableCell>
                                 <TableCell align="right">Interest Rate</TableCell>
+                                <TableCell align="right">Closing Costs</TableCell>
                                 <TableCell align="right">Monthly Payment</TableCell>
                                 <TableCell align="right">APR</TableCell>
                             </TableRow>
@@ -93,16 +155,16 @@ export default class App extends Component {
                                 <TableRow>
                                     <TableCell align="right">{row.lenderName}</TableCell>
                                     <TableCell align="right">{row.loanType}</TableCell>
-                                    <TableCell align="right">{row.interestRate}</TableCell>
-                                    <TableCell align="right">{row.closingCosts}</TableCell>
-                                    <TableCell align="right">{row.monthlyPayment}</TableCell>
-                                    <TableCell align="right">{row.apr}</TableCell>
+                                    <TableCell align="right">{parseFloat(row.interestRate).toFixed(3) + '%'}</TableCell>
+                                    <TableCell align="right">{'$' + parseFloat(row.closingCosts).toFixed(2)}</TableCell>
+                                    <TableCell align="right">{'$' + parseFloat(row.monthlyPayment).toFixed(2)}</TableCell>
+                                    <TableCell align="right">{parseFloat(row.apr).toFixed(3) + '%'}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-            </div>
+            </form>
         )};
     }
 
